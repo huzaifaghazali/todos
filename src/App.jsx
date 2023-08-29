fbdc6cc11da65cfba4c4ce733b8e9febecddd290
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-29 19:08:52 +0500

    Added Todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import CssBaseline from "@mui/material/CssBaseline";
+import TodoList from './TodoList';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <CssBaseline />
       <h1>Todo</h1>
+      <TodoList />
     </>
   )
 }
