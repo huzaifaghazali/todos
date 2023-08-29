fca0d20e55bfc2d707b76f7a895e62bf98e42df7
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-29 18:33:48 +0500

    Added mui package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <CssBaseline />
       <h1>Todo</h1>
     </>
   )
