cf5649ffd38b02e0a70a0641527904f19dec19c9
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-30 18:15:40 +0500

    Updated code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
   return (
@@ -18,7 +17,7 @@ export default function Navbar() {
             aria-label='menu'
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+
           </IconButton>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             React Todos
