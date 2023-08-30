271014b49339fab5bdeb1d74ef9ed98827377471
huzaifaghazali <huzaifaghazali@gmail.com>
2023-08-30 18:41:12 +0500

    Updated code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -7,15 +6,13 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'unused-imports'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "unused-imports/no-unused-imports": "error",
+    'react-refresh/only-export-components': 'warn',
+    'simple-import-sort/imports': 'error',
     'react/prop-types': 'off',
   },
-};
+}
